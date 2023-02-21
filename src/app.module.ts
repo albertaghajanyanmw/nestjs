7c25e473cac656fa087cb446849051358d658acd
Albert Aghajanyan <albert.aghajanyan.mw@gmail.com>
2023-02-22 00:28:26 +0400

    Added Users

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Dialect } from 'sequelize';
+import { User } from './users/users.model';
 import { UsersModule } from './users/users.module';
 
 @Module({
@@ -16,7 +17,7 @@ import { UsersModule } from './users/users.module';
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
-      models: [],
+      models: [User],
       autoLoadModels: true,
     }),
     UsersModule,
---
+++ src/users/users.model.ts
@@ -0,0 +1,50 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Column, DataType, Model, Table } from 'sequelize-typescript';
+
+interface UserCreationAttr {
+  email: string;
+  password: string;
+}
+
+@Table({ tableName: 'users' })
+export class User extends Model<User, UserCreationAttr> {
+  @ApiProperty({ example: '1', description: 'Unique id' })
+  @Column({
+    type: DataType.INTEGER,
+    unique: true,
+    autoIncrement: true,
+    primaryKey: true,
+  })
+  id: number;
+
+  @ApiProperty({ example: 'First name', description: 'User first name' })
+  @Column({ type: DataType.STRING, allowNull: true })
+  firstName: string;
+
+  @ApiProperty({ example: 'Last name', description: 'User last name' })
+  @Column({ type: DataType.STRING, allowNull: true })
+  lastName: string;
+
+  @ApiProperty({
+    example: 'test@yopmail.com',
+    description: 'User email address',
+  })
+  @Column({ type: DataType.STRING, unique: true, allowNull: false })
+  email: string;
+
+  @ApiProperty({ example: '11111', description: 'User password' })
+  @Column({ type: DataType.STRING, allowNull: false })
+  password: string;
+
+  @ApiProperty({ example: 'Last name', description: 'User last name' })
+  @Column({ type: DataType.STRING, unique: false, allowNull: true })
+  phone: string;
+
+  @ApiProperty({ example: true, description: 'User active state' })
+  @Column({ type: DataType.BOOLEAN, allowNull: false, defaultValue: true })
+  isActive: boolean;
+
+  @ApiProperty({ example: false, description: 'User archived' })
+  @Column({ type: DataType.STRING, allowNull: false, defaultValue: false })
+  archived: boolean;
+}
